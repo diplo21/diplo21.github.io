b4498ad4ae3437b767b54dbbcfde3f64607181d1
diplo21 <144032309+diplo21@users.noreply.github.com>
2025-04-18 08:41:00 +0200

    Update and rename veille.html to footer.php

---
+++ footer.php
@@ -0,0 +1,54 @@
+
+  <!-- Inclusion des fichiers JavaScript nécessaires -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <!-- Rubrique d'informations -->
+  <section class="info_section layout_padding">
+    <div class="container">
+      <div class="info_logo">
+        <h2> Diplo Shop </h2>
+      </div>
+      <div class="info_contact">
+        <div class="row">
+          <div class="col-md-4">
+            <a href="#">
+              <img src="images/location.png" alt="Emplacement">
+              <span> Av. Gustave Eiffel, 75007 Paris </span>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a href="#">
+              <img src="images/call.png" alt="Appel">
+              <span> Appel : +33760097725 </span>
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a href="#">
+              <img src="images/mail.png" alt="Email">
+              <span> princenzenzeki270@gmail.com </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8 col-lg-9">
+          <div class="info_form">
+            <form action="#">
+              <input type="text" placeholder="Entrez votre email" aria-label="Entrez votre email">
+              <button> S'enregistrer </button>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-3">
+          <div class="info_social">
+            <a href="#"><img src="images/facebook-logo-button.png" alt="Facebook"></a>
+            <a href="#"><img src="images/twitter-logo-button.png" alt="Twitter"></a>
+            <a href="#"><img src="images/linkedin.png" alt="LinkedIn"></a>
+            <a href="#"><img src="images/instagram.png" alt="Instagram"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Fin de la section d'informations -->
